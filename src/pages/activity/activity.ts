553181b5c7b9df93ea83128de1382a1a895b03d9
Lmilne <lmilne360@gmail.com>
2017-05-25 00:52:05 -0400

    Changed add activity method to use a prompt

--- src/pages/activity/activity.ts
+++ src/pages/activity/activity.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-activity',
@@ -10,13 +11,48 @@ export class ActivityPage {
 	activities: any[]
 	myDate: String = new Date().toISOString();
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
   	this.activities = [ {name: "Chest", date: this.myDate }]
   }
 
+/** Old addActivity method
   addActivity(){
   	let activity = {name: "New Activity", date: this.myDate}
   	this.activities.push(activity)
   	console.log(activity)
   }
+*/
+
+/* This function raises a prompt for user to input activity name */
+  addActivity(){
+    let prompt= this.alertCtrl.create({
+      title: 'New Activity',
+      message: "Enter a name for this activity",
+      inputs: [
+      {
+        name: 'name',
+        placeholder: 'Name'
+      }],
+
+      buttons: [
+      {
+        text: 'Cancel',
+        handler: data =>{
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          data.date = this.myDate
+          this.activities.push(data)
+          console.log(data)
+          console.log('Save clicked');
+        }
+      }
+      ]
+    });
+    prompt.present();
+  }
+
 }
