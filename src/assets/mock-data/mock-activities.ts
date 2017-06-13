7dad0de8698e6c41f95799ab7d82ba4a3ebcdea1
Lmilne <lmilne360@gmail.com>
2017-06-13 19:36:01 -0400

    added date to mock activity

--- src/assets/mock-data/mock-activities.ts
+++ src/assets/mock-data/mock-activities.ts
@@ -1,6 +1,6 @@
 export const ACTIVITIES: any[] = [
-{title: "Tiki Birthday", date: 'none', description: "A birthday party for our dear friend tiki"},
-{title: "Drinks", date: 'none', description: "Time to get wasted"},
-{title: "Lunch Special", date: 'none', description: "Food, glorious food"},
-{title: "Gaming event", date: 'none'}
+{title: "Tiki Birthday", date: new Date().toISOString(), description: "A birthday party for our dear friend tiki"},
+{title: "Drinks", date: new Date().toISOString(), description: "Time to get wasted"},
+{title: "Lunch Special", date: new Date().toISOString(), description: "Food, glorious food"},
+{title: "Gaming event", date: new Date().toISOString()}
 ]
